3a6efa00229ff43404e93373536f539221efe080
Paul S. <pavelslabunov@icloud.com>
2026-06-21 20:59:42 +0200

    docs: Add Javadoc comments for API exception classes

--- dlibs-api/src/main/java/me/demro/dlibs/api/exceptions/APIException.java
+++ dlibs-api/src/main/java/me/demro/dlibs/api/exceptions/APIException.java
@@ -2,12 +2,30 @@ package me.demro.dlibs.api.exceptions;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Base exception for all DBans API errors.
+ *
+ * @since 1.0.0
+ */
 public class APIException extends RuntimeException {
 
+    /**
+     * Creates exception with message.
+     *
+     * @param message error message
+     * @since 1.0.0
+     */
     public APIException(@NotNull String message) {
         super(message);
     }
 
+    /**
+     * Creates exception with message and cause.
+     *
+     * @param message error message
+     * @param cause   cause
+     * @since 1.0.0
+     */
     public APIException(@NotNull String message, @NotNull Throwable cause) {
         super(message, cause);
     }
