99419a869ba690f3efd315389eecfbf25087bb3b
Paul S. <pavelslabunov@icloud.com>
2026-06-21 20:59:32 +0200

    docs: Add Javadoc comments for punishment event classes

--- dlibs-api/src/main/java/me/demro/dlibs/api/events/PunishmentExpireEvent.java
+++ dlibs-api/src/main/java/me/demro/dlibs/api/events/PunishmentExpireEvent.java
@@ -3,8 +3,19 @@ package me.demro.dlibs.api.events;
 import me.demro.dlibs.api.Punishment;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Fired when a temporary punishment expires naturally.
+ *
+ * @since 1.0.0
+ */
 public class PunishmentExpireEvent extends PunishmentEvent {
 
+    /**
+     * Creates event for given expired punishment.
+     *
+     * @param punishment expired punishment
+     * @since 1.0.0
+     */
     public PunishmentExpireEvent(@NotNull Punishment punishment) {
         super(punishment);
     }
--- dlibs-api/src/main/java/me/demro/dlibs/api/events/PunishmentRevokeEvent.java
+++ dlibs-api/src/main/java/me/demro/dlibs/api/events/PunishmentRevokeEvent.java
@@ -3,8 +3,19 @@ package me.demro.dlibs.api.events;
 import me.demro.dlibs.api.Punishment;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Fired when a punishment is revoked via command or API.
+ *
+ * @since 1.0.0
+ */
 public class PunishmentRevokeEvent extends PunishmentEvent {
 
+    /**
+     * Creates event for given revoked punishment.
+     *
+     * @param punishment revoked punishment
+     * @since 1.0.0
+     */
     public PunishmentRevokeEvent(@NotNull Punishment punishment) {
         super(punishment);
     }
